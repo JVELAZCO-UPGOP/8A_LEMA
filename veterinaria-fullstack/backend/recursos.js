52d1a9750b40d341188517a0291c2dd241e1392e
luisavila <luisavila.@gmail.com>
2021-04-18 21:16:25 -0500

    part 11.1

--- veterinaria-fullstack/backend/recursos.js
+++ veterinaria-fullstack/backend/recursos.js
@@ -1,7 +1,7 @@
 module.exports = { mascotas: [
-    { tipo: "Perro", nombre: "Trosky0", dueno: "luis"},
-    { tipo: "Perro", nombre: "Trosky1", dueno: "Enrique"},
-    { tipo: "Perro", nombre: "Trosky2", dueno: "Eren"},
+    { tipo: "Perro", nombre: "Trosky0", dueno: "Luis"},
+    { tipo: "Perro", nombre: "Trosky1", dueno: "Francisco"},
+    { tipo: "Perro", nombre: "Trosky2", dueno: "Ricardo"},
     ],
     veterinarias: [
         { nombre: "Alexandra", apellido: "Perez", documento: "1234567890"},
@@ -9,10 +9,10 @@ module.exports = { mascotas: [
         { nombre: "Luis", apellido: "Avila", documento: "123456670"},
         ],
         duenos: [
-        { nombre: "Alejandra", apellido: "Perez", documento: "12234567890"},
-        { nombre: "Manuel", apellido: "Gomez", documento: "123445670"},
-        { nombre: "Enrique", apellido: "Avila", documento: "1234566670"},
+        { nombre: "Luis", apellido: "Perez", documento: "12234567890"},
+        { nombre: "Francisco", apellido: "Gomez", documento: "123445670"},
+        { nombre: "Ricardo", apellido: "Avila", documento: "1234566670"},
     ],
     consultas: [
-{mascota: 0, veterinaria: 0, encabezado: "", fechaCreacion: new Date(), fechaEdicion: new Date(), historia: "Historia", diagnostico: "diagnostico"},],
+{mascota: 0, veterinaria: 0, encabezado: "", fechaCreacion: new Date(), fechaEdicion: new Date(), historia: "Historia", diagnostico: "Moquillo"},],
     };
