f471b103d2c0901836a7fa7ff4b4c24facdef2db
luisavila <luisavila.@gmail.com>
2021-04-25 18:47:46 -0500

    part 13.1

---
+++ node-react/backend/recursos.js
@@ -0,0 +1,18 @@
+module.exports = { mascotas: [
+    { tipo: "Perro", nombre: "Trosky0", dueno: "Luis"},
+    { tipo: "Perro", nombre: "Trosky1", dueno: "Francisco"},
+    { tipo: "Perro", nombre: "Trosky2", dueno: "Ricardo"},
+    ],
+    veterinarias: [
+        { nombre: "Alexandra", apellido: "Perez", documento: "1234567890"},
+        { nombre: "Alexander", apellido: "Gomez", documento: "12345670"},
+        { nombre: "Luis", apellido: "Avila", documento: "123456670"},
+        ],
+        duenos: [
+        { nombre: "Luis", apellido: "Perez", documento: "12234567890"},
+        { nombre: "Francisco", apellido: "Gomez", documento: "123445670"},
+        { nombre: "Ricardo", apellido: "Avila", documento: "1234566670"},
+    ],
+    consultas: [
+{mascota: 0, veterinaria: 0, encabezado: "", fechaCreacion: new Date(), fechaEdicion: new Date(), historia: "Historia", diagnostico: "Moquillo"},],
+    };
